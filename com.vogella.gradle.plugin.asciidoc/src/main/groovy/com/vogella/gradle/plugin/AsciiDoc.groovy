7a286f7b714ddb177085f957b63b07a8698676ca
Fabian Pfaff <fap-@users.noreply.github.com>
2016-11-17 05:45:43 +0100

    Break build on certain asciidoctor warnings

--- com.vogella.gradle.plugin.asciidoc/src/main/groovy/com/vogella/gradle/plugin/AsciiDoc.groovy
+++ com.vogella.gradle.plugin.asciidoc/src/main/groovy/com/vogella/gradle/plugin/AsciiDoc.groovy
@@ -1,7 +1,10 @@
 package com.vogella.gradle.plugin
 
 import org.asciidoctor.gradle.AsciidoctorTask
+import org.gradle.api.GradleException
 import org.gradle.api.tasks.TaskAction
+import org.gradle.logging.internal.OutputEvent
+import org.gradle.logging.internal.OutputEventListener
 
 class AsciiDoc extends AsciidoctorTask {
 
@@ -27,22 +30,49 @@ class AsciiDoc extends AsciidoctorTask {
 		'pdf-style':'basic'
 	}
 
-	@TaskAction
-	def renameFile() {
-		renameFile(new File("${project.buildDir}"))
+	def renameFiles() {
+		renameFiles(new File("${project.buildDir}"))
 	}
 	
 	def matchesFilePattern(fileName) {
 		fileName.startsWith("001_article") || fileName.startsWith('001_book')
 	}
 
-	def renameFile(def buildDir) {
+	def renameFiles(def buildDir) {
 		buildDir.eachFileRecurse( {
 			if(matchesFilePattern(it.name)){
 				it.renameTo(new File(it.parent, it.name.substring(4, it.name.length())))
 			}
 		}
 		)
+	}
+	
+	@TaskAction
+	void processAsciidocSources() {
+		def outputEvents = []
+		def listener = addOutputEventListener(outputEvents)
+		super.processAsciidocSources()
+		renameFiles()
+		getLogging().removeOutputEventListener(listener)
+		checkForRelevantWarnings(outputEvents)
+	}
+	
+	def addOutputEventListener(outputEvents) {
+		def listener = [onOutput: {event -> outputEvents << event}] as OutputEventListener
+		getLogging().addOutputEventListener(listener)
+		listener
+	}
+	
+	def checkForRelevantWarnings(outputEvents) {
+		outputEvents.each { e ->
+			if (matchesRelevantWarning(e.toString())) {
+				throw new GradleException("You have some asciidoctor warnings, please fix them!");
+			}
+		}
+	}
+	
+	def matchesRelevantWarning(e) {
+		e =~ "include file not found" || e =~ "only book doctypes can contain level 0 sections" || e =~ "section title out of sequence"
 	}
 }
 
