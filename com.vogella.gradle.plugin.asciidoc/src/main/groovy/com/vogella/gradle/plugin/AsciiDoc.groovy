ccca5f2860a631a78645a1e3907fb919c3335e10
Fabian Pfaff <fap-@users.noreply.github.com>
2016-11-24 03:43:36 +0100

    Backport asciidoctor warning check to 0.5.x

--- com.vogella.gradle.plugin.asciidoc/src/main/groovy/com/vogella/gradle/plugin/AsciiDoc.groovy
+++ com.vogella.gradle.plugin.asciidoc/src/main/groovy/com/vogella/gradle/plugin/AsciiDoc.groovy
@@ -1,6 +1,8 @@
 package com.vogella.gradle.plugin
 
 import org.asciidoctor.gradle.AsciidoctorTask
+import org.gradle.api.GradleException
+import org.gradle.api.logging.StandardOutputListener
 import org.gradle.api.tasks.TaskAction
 
 class AsciiDoc extends AsciidoctorTask {
@@ -21,6 +23,16 @@ class AsciiDoc extends AsciidoctorTask {
 		'docinfo1':'true',
 		'pdf-stylesdir':'../themes',
 		'pdf-style':'basic'
+		
+		def capturedOutput = []
+		def listener = { capturedOutput << it } as StandardOutputListener
+		logging.addStandardOutputListener(listener)
+		logging.addStandardErrorListener(listener)
+		doLast {
+			logging.removeStandardOutputListener(listener)
+			logging.removeStandardErrorListener(listener)
+			checkForRelevantWarnings(capturedOutput)
+		}
 	}
 
 	@TaskAction
@@ -36,5 +48,17 @@ class AsciiDoc extends AsciidoctorTask {
 		}
 		)
 	}
+	
+	def checkForRelevantWarnings(outputEvents) {
+		outputEvents.each { e ->
+			if (matchesRelevantWarning(e.toString())) {
+				throw new GradleException("You have some asciidoctor warnings, please fix them!");
+			}
+		}
+	}
+	
+	def matchesRelevantWarning(e) {
+		e =~ "include file not found" || e =~ "only book doctypes can contain level 0 sections" || e =~ "section title out of sequence"
+}
 }
 
