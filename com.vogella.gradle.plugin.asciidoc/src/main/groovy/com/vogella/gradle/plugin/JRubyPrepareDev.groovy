10fedc916d01fa58e8c16875d85747c198a43a2d
Fabian Pfaff <fabian.pfaff@vogella.com>
2017-06-13 13:53:37 +0200

    Adds new version of asciidoctor-pdf with physical-page-numbers attribute

--- com.vogella.gradle.plugin.asciidoc/src/main/groovy/com/vogella/gradle/plugin/JRubyPrepareDev.groovy
+++ com.vogella.gradle.plugin.asciidoc/src/main/groovy/com/vogella/gradle/plugin/JRubyPrepareDev.groovy
@@ -9,7 +9,7 @@ class JRubyPrepareDev extends JRubyPrepare {
 
     public JRubyPrepareDev() {
 		outputDir "${project.buildDir}/jruby_prepare"
-		localDependency(new Gem(name: "asciidoctor-pdf-1.5.0.alpha.16.dev.1", jrubyPrepare: this))
+		localDependency(new Gem(name: "asciidoctor-pdf-1.5.0.alpha.16.dev.2", jrubyPrepare: this))
 		localDependency(new Gem(name: "asciidoctor-1.5.6.dev.2", jrubyPrepare: this))
 		copyDevGems(localDependencies)
 		dependencies project.configurations.gems
