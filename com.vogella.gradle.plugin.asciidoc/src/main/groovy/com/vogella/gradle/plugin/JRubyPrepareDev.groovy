3f06d4d70efbe14be6d93f00662ee5cc85562b94
Fabian Pfaff <fabian.pfaff@vogella.com>
2017-08-04 18:05:58 +0200

    Moves jrubyPrepare gems to project main directory

--- com.vogella.gradle.plugin.asciidoc/src/main/groovy/com/vogella/gradle/plugin/JRubyPrepareDev.groovy
+++ com.vogella.gradle.plugin.asciidoc/src/main/groovy/com/vogella/gradle/plugin/JRubyPrepareDev.groovy
@@ -8,7 +8,8 @@ class JRubyPrepareDev extends JRubyPrepare {
 	List<Gem> localDependencies = []
 
     public JRubyPrepareDev() {
-		outputDir "${project.buildDir}/jruby_prepare"
+		def buildDir = project.parent ? project.parent.buildDir : project.buildDir
+		outputDir "${buildDir}/jruby_prepare"
 		localDependency(new Gem(name: 'asciidoctor-pdf-1.5.0.alpha.17.dev.1', jrubyPrepare: this))
 		doLast {
 			copyDevGems(localDependencies)
