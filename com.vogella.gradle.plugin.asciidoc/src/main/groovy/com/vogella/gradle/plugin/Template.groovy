d147be7ee307b19f84c05d94f456d2c2279f157f
Simon Scholz <simon.scholz@vogella.com>
2019-02-15 15:57:30 +0100

    Use the gradle plugin plugin and updated the template task
    Change-Id: Ieb8de7b492383953491fbcf604bece75d322343e
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.gradle.plugin.asciidoc/src/main/groovy/com/vogella/gradle/plugin/Template.groovy
+++ com.vogella.gradle.plugin.asciidoc/src/main/groovy/com/vogella/gradle/plugin/Template.groovy
@@ -2,13 +2,35 @@ package com.vogella.gradle.plugin
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.options.Option
+
+import groovy.transform.CompileStatic
 
 class Template extends DefaultTask {
 
+	private String projectName;
+
+	public Template() {
+		description = 'Creates a template for vogella tutorials'
+		group = 'Documentation'
+	}
+	
 	@TaskAction
 	def createTemplate() {
-		if(project.projectDir.list().length <= 0) {
+		if(this.projectName != null) {
+			new File("${projectName}/img").mkdirs()
+			new File("${projectName}/res").mkdirs()
+			
+			createArticleFile()
+			createResourcesFile()
+			createOverviewFile()
+			
+			addIncludeToSettings()
+		}
+		else if(project.projectDir.list().length <= 0) {
+			this.projectName = project.projectDir.name;
 			new File('img').mkdir()
 			new File('res').mkdir()
 
@@ -19,13 +41,19 @@ class Template extends DefaultTask {
 			throw new GradleException("The ${project.projectDir} directory, which should be initialized, must be empty")
 		}
 	}
+	
+	@Option(option = 'name', description = 'Specify a name for the new project')
+	void setFile(final String projectName) {
+		this.projectName = projectName;
+	}
 
 	def createArticleFile() {
 		def date = new Date();
 		def now = new java.text.SimpleDateFormat("dd.MM.yyyy").format(date)
 		def year = new java.text.SimpleDateFormat("yyyy").format(date)
-		def userName = project.hasProperty('userName') ? project.property('userName') + ' ' : ''
-		project.file('001_article.adoc') << """= ${project.projectDir.name} - Tutorial
+		def userName = project.hasProperty('userName') ? String.valueOf(project.property('userName')) : ''
+		project.file("${projectName}/001_article.adoc") << """= ${projectName} - Tutorial
+:toc:
 :linkcss:
 :sectnums:
 :experimental:
@@ -34,28 +62,39 @@ class Template extends DefaultTask {
 ${userName}(c) ${year} vogella GmbH
 Version 0.1, ${now}
 :docinfodir: ../
-:vgwort: ${project.projectDir.name}
+:vgwort: ${projectName}
 
 [abstract]
-This tutorial contains notes about ${project.projectDir.name}.
+This tutorial contains notes about ${projectName}.
 
 include::010_overview.adoc[]
 include::../web/advertisement/inBetween01.adoc[]
+
 include::../web/advertisement/inBetween02.adoc[]
-include::../support.adoc[]
 include::008_resourceslocal.adoc[]
 include::../copyright.adoc[]
+
 
 """
 	}
 	def createResourcesFile() {
-		project.file('008_resourceslocal.adoc') << """== ${project.projectDir.name} resources
+		project.file("${projectName}/008_resourceslocal.adoc") << """== ${projectName} resources
 
 """
 	}
 	def createOverviewFile() {
-		project.file('010_overview.adoc') << """== ${project.projectDir.name}
+		project.file("${projectName}/010_overview.adoc") << """== ${projectName}
 
 """
+	}
+	
+	def addIncludeToSettings() {
+		def settingsFile = project.file('settings.gradle')
+		if(settingsFile) {
+			settingsFile << """
+include '${projectName}'"""
+		} else {
+			throw new GradleException("The settings.gradle file does not exist.")
+		}
 	}
 }
